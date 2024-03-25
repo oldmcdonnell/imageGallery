e1aae9d9a2ce80a9a9b78eaf85016d5f019db9f0
oldmc <oldmcdonnell@gmail.com>
2024-03-25 17:24:57 -0400

    adding image containers

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const directoryPath = '../img/';
+  
+const imgFiles = [];
+
 function getFiles(directoryPath, imgFiles) {
     const filesInDirectory = fs.readdirSync(directoryPath);
   
@@ -20,17 +24,27 @@ function getFiles(directoryPath, imgFiles) {
     }
   }
   
-  const directoryPath = '../img/';
-  
-  const imgFiles = [];
-  
   getFiles(directoryPath, imgFiles);
   
   console.log(imgFiles);
 
+//image container?
+const container = document.querySelector("#image-container");
+
+imgFiles.forEach((imgFiles) => {
+  const img = document.createElement("img");
+  img.src = imgFiles;
+  container.appendChild(img);
+});
+
+
+
 /* Declaring the alternative text for each image file */
 
 //alt text replace function for items in imgFiles, replace "_" with " " so "Closeup_of_an_eye.jpg" becomes "Closeup of an eye"
+//const filename = "This_is_a_closeup_of_an_eye.jpg";
+//altText = filename.replaceAll("_", " ");
+//console.log(altText.replace(".jpg", ""));
 
 /* Looping through images */
 
