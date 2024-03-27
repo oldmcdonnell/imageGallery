7bbfadf2dd3e71ef422745ae0a6cfb026f9a6242
oldmc <oldmcdonnell@gmail.com>
2024-03-27 13:26:15 -0400

    js cleaning up

--- js/main.js
+++ js/main.js
@@ -8,14 +8,34 @@ const overlay = document.querySelector('.overlay');
 
 const imgFiles = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 
+/*
+const fs = require('fs');
+
+// Get the directory path
+const directoryPath = './images';
+
+// Create an empty array to store the image paths
+const imagePaths = [];
+
+// Read the directory contents
+fs.readdirSync(directoryPath).forEach(file => {
+  // Check if the file is an image
+  if (file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.gif')) {
+    // Add the image path to the array
+    imagePaths.push(`${directoryPath}/${file}`);
+  }
+});
+
+*/
+
 /* Declaring the alternative text for each image file */
 
 const alts = {
-    'pic1.jpg' : 'Closeup of a human eye',
-    'pic2.jpg' : 'Rock that looks like a wave',
-    'pic3.jpg' : 'Purple and white pansies',
-    'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-    'pic5.jpg' : 'Large moth on a leaf'
+    'pic1.jpg' : 'Weird eye image',
+    'pic2.jpg' : 'Foot of a stone gargoyle',
+    'pic3.jpg' : 'Purple flowers',
+    'pic4.jpg' : 'Heiroglyhics',
+    'pic5.jpg' : 'Fuzzy moth friend'
   }
 
 
@@ -25,10 +45,14 @@ const alts = {
 //console.log(altText.replace(".jpg", ""));
 
 /* Looping through images */
-
+for (image of imgFiles){
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
+newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 /* Wiring up the Darken/Lighten button */
